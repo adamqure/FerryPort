67ae1ab1d5a334beb680d1383e9f6134fce9baac
Adam Ure <adamqure@icloud.com>
2021-11-10 17:14:58 -0700

    Initial Commit

---
+++ FerryPortChallenge/FerryPortChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FerryPortChallengeApp.swift
+//  FerryPortChallenge
+//
+//  Created by Adam Ure on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct FerryPortChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FerryPortChallengeUITests/FerryPortChallengeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FerryPortChallengeUITestsLaunchTests.swift
+//  FerryPortChallengeUITests
+//
+//  Created by Adam Ure on 11/10/21.
+//
+
+import XCTest
+
+class FerryPortChallengeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
